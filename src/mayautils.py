eb8cadbf80b466921174145175973d8173930cd9
asmith564 <aaronsmith0709@gmail.com>
2020-06-20 18:12:12 -0500

    Updated directory with a source file

--- src/mayautils.py
+++ src/mayautils.py
@@ -1,4 +1,13 @@
+import logging
+import os
+
 import pymel.core as pmc
+from   pymel.core.system import Path
+
+from pymel.core.system import versions
+
+log = logging.getLogger(__name__)
+
 
 def main():
     counter = 0
@@ -10,24 +19,31 @@ def main():
         cubexform.translateY.set(1.5 * counter)  # translate 1.5*counter units
         counter += 1
 
+
 class SceneFile(object):
     """Class used to represent a DCC software scene file
 
     can be used to manipulate scene files without needing direct influence on the scene
 
     Attributes:
-    dir(str, optional): Directory to the scene file, defaults to none
+    dir(str, optional): Directory to the scene file, defaults to ''
     descriptor(str, optional): Short descriptor of the scene file, defaults to main
     version (int, optional): Version number, defaults to 1
     ext (str, optional): extension defaults to "ma"
     """
-    def __init__(self, dir=None, descriptor='main', version=1, ext="ma"):
-        self.dir = dir
+    def __init__(self, dir='', descriptor='main', version=1, ext="ma"):
+        self._dir = Path(dir)
         self.descriptor = descriptor
         self.version = version
         self.ext = ext
 
+    @property
+    def dir(self):
+        return self._dir
 
+    @dir.setter
+    def dir(self, val):
+        self._dir = Path(val)
 
     def basename(self):
         """Returns the DCC scene file's name
@@ -37,3 +53,41 @@ class SceneFile(object):
         name_pattern = "{descriptor}_{version:03d}.{ext}"
         name = name_pattern.format(descriptor=self.descriptor, version=self.version, ext=self.ext)
         return name
+
+    def path(self):
+        """The function returns a path to scene file
+        includes drive letter, any directory path and the file name
+
+        Returns:
+            Path: The path to the scene file
+            """
+        return Path(self.dir) / self.basename()
+
+    def save(self):
+        """Saves the scene file.
+
+        Returns:
+            :obj:'Path': The path to the scene file if successful, None, otherwise
+            """
+        try:
+            pmc.system.saveAs(self.path())
+        except RuntimeError:
+            log.warning("Missing directories. creating directories")
+            self.dir.makedirs_p()
+            pmc.system.saveAs(self.path())
+
+    def increment_and_save(self):
+        CurrentVersion = 1
+        for f in self.dir.files('*.ma'):
+            parts = os.path.split(f)
+            Directory = parts[0]
+            Name = parts[1].split('_v')
+            Descriptor = Name[0]
+            Split2 = Name[1].split('.')
+            Version = int(Split2[0])
+            Extension = Split2[1]
+            if CurrentVersion < Version:
+                CurrentVersion = Version
+        CurrentVersion = CurrentVersion + 1
+        Path = Directory + "\\" + Descriptor + '_v0' + str(CurrentVersion) + '.' + Extension
+        pmc.system.saveAs(Path)
